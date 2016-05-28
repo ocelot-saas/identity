8377d8886345b14b76e6ca25378691217a18f38c
Horia Coman <horia141@gmail.com>
2016-05-28 18:15:47 +0300

    New porting.

--- src/identity/config.py
+++ src/identity/config.py
@@ -3,7 +3,7 @@ import os
 
 
 address = '0.0.0.0'
-port = int(os.environ['PORT'])
+port = int(os.environ.get('PORT', '10000'))
 bind = '{}:{}'.format(address, port)
 workers = multiprocessing.cpu_count() * 2 + 1
 accesslog = '-'
