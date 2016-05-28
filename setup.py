c6cf3978ab621edc9e8bf18662e7975cf68a92fd
Horia Coman <horia141@gmail.com>
2016-05-28 12:38:20 +0300

    Add proper parsing of local listening port in Heroku.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def readme():
 
 setup(
     name='identity',
-    version='0.0.3',
+    version='0.0.4',
     description='The identity service for Ocelot, as a Python package.',
     long_description=readme(),
     keywords='ocelot identity service rest api',
--- src/identity/config.py
+++ src/identity/config.py
@@ -1,7 +1,9 @@
 import multiprocessing
+import os
+
 
 address = '0.0.0.0'
-port = 80
+port = int(os.environ['PORT'])
 bind = '{}:{}'.format(address, port)
 workers = multiprocessing.cpu_count() * 2 + 1
 accesslog = '-'
