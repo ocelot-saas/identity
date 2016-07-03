24a5a425693557aaf69ad13eb3e35ff135a297f5
Horia Coman <horia141@gmail.com>
2016-07-03 12:22:54 +0300

    Dropped encryption for gemfury.

--- bin/togemfury.sh
+++ bin/togemfury.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 python setup.py sdist
-curl -s -F package=@dist/`ls dist` https://$GEMFURY_KEY@push.fury.io/ocelot-saas/ > result
+curl -s -F package=@dist/`ls dist` https://vsRAKKMwEs5p1RhfMGiF@push.fury.io/ocelot-saas/ > result
 if [ -z "$(grep -e ok result)" ]
 then
   exit 1
